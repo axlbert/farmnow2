d9020c65c9570735694e3eafabf6342ff6eacbc3
axlbert <aclanger131@gmail.com>
2016-11-30 23:07:27 +0100

    buyer portal

---
+++ script.js
@@ -0,0 +1,134 @@
+
+var iniLocations = [{
+  clickCount: 0,
+  name: "Solenoid",
+  descr: "a great view",
+  article : ko.observable(),
+  coords: {
+    lat: 50.364409,
+    lng: 7.614130
+  }
+}, {
+  clickCount: 0,
+  name: "Pressure Relief",
+  descr: "an awesome green",
+  article : ko.observable(),
+  coords: {
+    lat: 50.355523,
+    lng: 7.602743
+  }
+}, {
+  clickCount: 0,
+  name: "Shutoff Valve",
+  descr: "two rivers at once",
+  article : ko.observable(),
+  coords: {
+    lat: 50.364555,
+    lng: 7.606087
+  }
+}, {
+  clickCount: 0,
+  name: "Valve Isle",
+  descr: "a Fountain!",
+  article : ko.observable(),
+  coords: {
+    lat: 50.360200,
+    lng: 7.598058
+  }
+}, {
+  clickCount: 0,
+  name: "2-Solenoid",
+  descr: "trains",
+  article : ko.observable(),
+  coords: {
+    lat: 50.358886,
+    lng: 7.590677
+  }
+}, {
+  clickCount: 0,
+  name: "Solenoid Budget",
+  descr: "a nice building",
+  article : ko.observable(),
+  coords: {
+    lat: 50.360793,
+    lng: 7.595737
+  }
+}, {
+  clickCount: 0,
+  name: "Lorem Ipsum",
+  descr: "neat architecture",
+  article : ko.observable(),
+  coords: {
+    lat: 50.355461,
+    lng: 7.603140
+  }
+}, {
+  clickCount: 0,
+  name: "Steam Trap",
+  descr: "an old building",
+  article : ko.observable(),
+  coords: {
+    lat: 50.362062,
+    lng: 7.603766
+  }
+}, {
+  clickCount: 0,
+  name: "Steam Trap Pharma",
+  descr: "an old square",
+  article : ko.observable(),
+  coords: {
+    lat: 50.358631,
+    lng: 7.610564
+  }
+}];
+
+  /**ViewModel aka Octopus*/
+
+var ViewModel = function() {
+    /**launching the google map*/
+    var self = this;
+    self.input = ko.observable('');    
+    self.filterList = ko.observableArray([]);
+
+    /** populating a copy of the locationlist with the dataset for better filtering*/
+    self.buildFilterList = function(val) {
+      iniLocations.forEach(function(val) {
+        self.filterList.push(val);
+      });
+    };
+
+    /** triggering the above mentioned*/
+    self.buildFilterList();
+
+
+    self.filterFunc = function() {
+      self.filterList.removeAll();
+      
+      var filterInput = self.input().toLowerCase();
+      if (filterInput.length > 0) {
+        //console.log("filter function is hit");
+        iniLocations.forEach(function(val) {
+          //self.markerArray.push(new self.constructMarkers(val));
+          if (val.name.toLowerCase().indexOf(filterInput) >= 0) {
+
+            //console.log("if clause of filter function is hit");
+            self.filterList.push(val);
+            /** using map center here causes some erratic behaviour*/
+            //map.setCenter(val.coords);
+          }
+        });
+      } else {
+       this.buildFilterList();
+      }
+
+    };
+
+};
+
+
+
+
+
+  $(document).ready(function(){
+ko.applyBindings(new ViewModel())
+});
